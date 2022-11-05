c025d33ae2dbaa7b71cfdfd50828cbfa57b027e4
Ariel Monti <djaroldito@gmail.com>
2022-11-05 11:33:36 -0300

    deploy7

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true}).then(() => {
+conn.sync({ force: false}).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('escuchando en puerto', process.env.PORT); // eslint-disable-line no-console
   });
